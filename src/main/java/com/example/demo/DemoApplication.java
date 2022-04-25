18f9dd894831e1c4eb807782ffb5eff65c21a485
Julian <julian.beutin@viacesi.fr>
2022-04-25 20:49:38 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@SpringBootApplication
+@RestController
+
+public class DemoApplication {
+
+	private final AtomicLong counter = new AtomicLong();
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
+		return new Greeting(counter.incrementAndGet(), "coucou");
+	}
+
+
+
+}
